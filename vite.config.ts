9a3209dd9e95538d0035b76b8b9dd4ff6a7f5094
Shiva Saxena <shivasaxena911@gmail.com>
2025-07-16 00:28:19 +0530

    fix: base CSS path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/handy-dev-tools/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
